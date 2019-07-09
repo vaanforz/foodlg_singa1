a3f8f1661e24c9d6c9a2622d9dcecc00c7e448c2
Vaan Ng <vaanforz@r-168-123-25-172.comp.nus.edu.sg>
2019-07-09 19:41:10 +0800

    changed run.sh path to $PWD and removed the mounting of external database folder

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-docker run -p 8085:5000 -v /hdd1/yisen/server-front-sep/database:/root/database -v /hdd1/yisen/server-front-sep/foodlg_singa1/:/root/foodlg -d foodlg-gpu
+docker run -p 8085:5000 -v $PWD:/root/foodlg -d foodlg-gpu
+
 
 
